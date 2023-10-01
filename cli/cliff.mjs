2a9289304bde1e91da3b58aa85bb3ab7fe7520d3
Try Ajitiono <ballinst@gmail.com>
2023-10-01 09:45:28 +0000

    attempt add custom commands

--- cli/cliff.mjs
+++ cli/cliff.mjs
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
 import meow from 'meow';
+import fs from 'fs/promises'
+import path from 'path';
+import os from 'os';
+
+async function getPossibleCustomCommands() {
+	try {
+		const entryJSONString = await fs.readFile(path.join(os.homedir(), '.imballinstack/cliff/entry.json'), 'utf-8')
+		const entryJSON = JSON.parse(entryJSONString)
+
+		return entryJSON
+	} catch (err) {
+		return null
+	}
+}
+
+console.info(getPossibleCustomCommands())
 
 const cli = meow(`
 	Usage
